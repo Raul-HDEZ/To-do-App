68cbe0094bd52cd7e406fb8c63d02f23aeba4132
Raul Hernandez <mjolnirk@localhost.localdomain>
2021-10-14 22:15:48 +0200

    14-Oct-2021

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'To-do';
+  task = ""
+  arraytasks: any[] = []
+
+  delTaskFromArray(element: string) {
+    this.arraytasks.forEach((value,index)=>{
+        if(value==element) this.arraytasks.splice(index,1);
+    });
+}
+
+  newTask(): void{
+    /* ... Operador Spread 
+    Crea un array a partir del array anterior añadiendole un elemento
+    USO ...(array-principal), (elemento-a-añadir) */
+    if (this.task != "") {
+      this.arraytasks = [...this.arraytasks, this.task] 
+      this.task = "";
+    }
+  }
+
+  delTask(taskToDelete: string){
+    this.delTaskFromArray(taskToDelete);
+  }
 }
